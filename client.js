ba32597f12205e1d13d7acad46b5b6e1c1728e80
derek@runsasoft.com <derek@runsasoft.com>
2020-11-01 23:46:08 +0800

    add test

--- client.js
+++ client.js
@@ -3,8 +3,12 @@ var container = require('runsa-rhea');
 const _ = require('lodash');
 
 var args = {
-    hosts: ['amqp01.nr01.runsasoft.com', '192.168.14.105'],
-    ports: [56233,5672]
+    // hosts:['192.168.14.105'],
+    // ports:[5672],
+    // hosts: ['amqp01.nr01.runsasoft.com','amqp01.nr01.runsasoft.com'],
+    // ports: [56233,56242],
+    hosts: ['47.110.224.59'],
+    ports: [61616]
 };
 
 var sender;
@@ -54,21 +58,53 @@ let receiver;
 function addReceiver(){
     if (receiver) return ;
     console.log('start open_receiver...');
-    receiver = connection.open_receiver('examples');
+    receiver = connection.open_receiver('rwin_trade::rwin_trade-all');
     receiver.on('receiver_open', () => {
         console.warn('receiver_open OK');  //not print this line
     });
-    receiver.on('message',()=>{
-        console.log('收到消息');
+    receiver.on('message',(ctx)=>{
+        console.log('收到消息',ctx.message);
     })
 }
 function addSender(){
     if (sender) return ;
     console.log('start open_sender...');
-    sender = connection.open_sender('examples');
-    sender.on('sendable', () => {
-        sender.send("123");
-        console.warn('sendable OK');  //not print this line
+    sender = connection.open_sender({
+        name: 'sender-' + Date.now(),
+        autosettle: true, //必须设置为true,否则只能发送2048个消息
+        target: {
+            address: 'rwin-trade',
+            timeout: 2
+        }, source: {timeout: 2}, timeout: 2
+        // snd_settle_mode:0 //0:发送到MQ,即收到accepted事件,1:收不到accepted事件
+    });
+    sender.on('settled',(ctx)=>{
+        console.debug(`connection.on(settled)`, '消息发送成功:', ctx.delivery.tag);
+    });
+    sender.on('sendable', (ctx) => {
+        sender.send({
+            message_id:Date.now(), //header.messageID
+            application_properties: {"app":"test"},
+            message_annotations: {}, //{ "x-opt-delivery-delay": 1000 /*延时消息*/},
+            body:JSON.stringify({a:123}),
+            // delivery_count: 8,//未体现
+            durable: 2 /*持久消息*/, //header.durable=true
+            //group_id:"group_id", //_AMQ_GROUP_ID消息分组发送到同一个consumer,(如果filter匹配不上,则会导致消息一直不能消费,直到consumer关闭后才能被其它consumer消费)
+            // user_id:'user_id', //JMSXUserID
+            // to: 'to', //未体现
+            // subject: 'subject', //JMSType
+            // reply_to:'reply_to', //JMSReplyTo
+            // correlation_id:'correlation_id', //JMSCorrelationID
+            // content_type:'content_type', //JMS_AMQP_ContentType
+            // content_encoding:'content_encoding', //JMS_AMQP_ContentEncoding
+            absolute_expiry_time: Date.now() + 3 * 24 * 60 * 60 * 1000, //header.expiration 过期时间
+            creation_time: Date.now(), //header.timestamp
+            // group_sequence: 77, //JMSXGroupSeq
+            // reply_to_group_id:'reply_to_group_id', //JMS_AMQP_ReplyToGroupID
+            priority: 3, //header.priority
+            first_acquirer: false
+        },Date.now().toString());
+        console.warn('sendable OK',sender.target.value[0].value); 
     });
 }
 // addReceiver();
@@ -87,7 +123,7 @@ connection.on('disconnected', function (context) {
 });
 
 connection.connect();
-addReceiver();
+// addReceiver();
 addSender();
 
 // console.warn('test 15672-->fail, 5672-->success');
